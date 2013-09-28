8dc7a449a4eef904848f856d0bd2596d59062769
unknown <Roboclub@Bumblebee.(none)>
2013-09-28 16:02:57 -0400

    09/28 logic switching between bools and high low needs work

--- MotorControl/MotorControl.ino
+++ MotorControl/MotorControl.ino
@@ -8,9 +8,16 @@
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
 int led = 13;
-int enablePin = 5;
+int inputRightSensor = 12;
+int enablePin = 6;
 int clockwiseControl = 2;
 int counterClockwiseControl = 1;
+
+const int SENSOR_SAFETY_DELAY = 10; // 10 us delay used to wait for sensor value
+
+const int SENSOR_TIMOUT = 4000; // 3 ms until sensor doesn't return value
+
+boolean isEnablePin, isClockwise, isCounterClockwise;
 
 // the setup routine runs once when you press reset:
 void setup() {                
@@ -19,24 +26,68 @@ void setup() {
   
   Serial.begin(9600);
   
-  pinMode(enablPin, OUTPUT);
-  digitalWrite(enablePin, HIGH);
+  pinMode(enablePin, OUTPUT);
+  isEnablePin = true;
   
-  pinMode(clockwiseContol, OUTPUT);
+  pinMode(clockwiseControl, OUTPUT);
+  isClockwise = true;
+  
   pinMode(counterClockwiseControl, OUTPUT);
+  isCounterClockwise = true;
+  
+  pinMode(inputRightSensor, INPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(counterClockWiseControl, HIGH);
-  Serial.print("Counterclockwise");
   
-  delay(1000);               // wait for a second
+  Serial.println(PingDigitalSensor(inputRightSensor));
   
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(counterClockwiseControl, LOW);
-  Serial.print("Off"); 
+  isCounterClockwise = false;
   
-  delay(1000);               // wait for a second
+  SetMotor(isClockwise, isCounterClockwise, isEnablePin);
+}
+
+void ChangeDirection()
+{
+}
+
+void SetMotor(boolean forward, boolean back, boolean isEnabled)
+{
+  digitalWrite(clockwiseControl, booleanToHighLow(forward));
+  digitalWrite(counterClockwiseControl, booleanToHighLow(back));
+  digitalWrite(enablePin, booleanToHighLow(isEnabled));
+}
+
+int booleanToHighLow(boolean toConvert)
+{
+  if(toConvert)
+    return HIGH;
+  return LOW;
+}
+
+int PingDigitalSensor(int pin) {
+  
+  // init values
+  long startTime;
+  int sensorTime;
+  
+  // ping sensor
+  pinMode( pin, OUTPUT );
+  digitalWrite( pin, HIGH );  
+  
+  // delay to safely switch to input mode
+  delayMicroseconds(SENSOR_SAFETY_DELAY);
+  pinMode( pin, INPUT );
+
+  // start timer (get current time, safe to use as overflows after 70 min)
+  startTime = micros();
+
+  // time how long the input is HIGH, but quit after 3ms as nothing happens after that
+  while ( (digitalRead(pin) == HIGH) && 
+          ((micros() - startTime) < SENSOR_TIMOUT) ); 
+  
+  sensorTime = micros() - startTime;
+
+  return sensorTime;
 }
